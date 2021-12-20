b36f33bc0e16f24d3d2b735d160f1aec160fb4a0
Tim Miller <innerlogic4321@gmail.com>
2021-12-20 11:01:10 -0500

    Add IMediaLibrary.cs

--- DrasticMedia.VLC/Library/MediaLibrary.cs
+++ DrasticMedia.VLC/Library/MediaLibrary.cs
@@ -19,7 +19,7 @@ namespace DrasticMedia.Core.Library
     /// <summary>
     /// Media Library.
     /// </summary>
-    public class MediaLibrary : IDisposable
+    public class MediaLibrary : IMediaLibrary
     {
         private ILogger logger;
         private IMusicDatabase musicDatabase;
@@ -54,67 +54,31 @@ namespace DrasticMedia.Core.Library
             }
         }
 
-        /// <summary>
-        /// Gets the new media item added event.
-        /// </summary>
+        /// <inheritdoc/>
         public event EventHandler<NewMediaItemEventArgs>? NewMediaItemAdded;
 
-        /// <summary>
-        /// Gets the new media item added event.
-        /// </summary>
+        /// <inheritdoc/>
         public event EventHandler<UpdateMediaItemEventArgs>? UpdateMediaItemAdded;
 
-        /// <summary>
-        /// Gets the remove media item event.
-        /// </summary>
+        /// <inheritdoc/>
         public event EventHandler<RemoveMediaItemEventArgs>? RemoveMediaItem;
 
-        /// <summary>
-        /// Gets the new media item error event.
-        /// </summary>
+        /// <inheritdoc/>
         public event EventHandler<NewMediaItemErrorEventArgs>? NewMediaItemError;
 
-        /// <summary>
-        /// Gets the music database.
-        /// </summary>
-        public IMusicDatabase MusicDatabase => this.musicDatabase;
-
-        /// <summary>
-        /// Gets the video database.
-        /// </summary>
-        public IVideoDatabase VideoDatabase => this.videoDatabase;
-
-        /// <summary>
-        /// Fetches all VideoItems.
-        /// </summary>
-        /// <returns>VideoItem.</returns>
+        /// <inheritdoc/>
         public Task<List<VideoItem>> FetchVideosAsync() => this.videoDatabase.FetchVideosAsync();
 
-        /// <summary>
-        /// Fetches all TV Shows.
-        /// </summary>
-        /// <returns>TVShows.</returns>
+        /// <inheritdoc/>
         public Task<List<TVShow>> FetchTVShowsAsync() => this.videoDatabase.FetchTVShowsAsync();
 
-        /// <summary>
-        /// Fetches a TV Show with episodes.
-        /// </summary>
-        /// <param name="id">TVShow id.</param>
-        /// <returns>TVShow.</returns>
+        /// <inheritdoc/>
         public Task<TVShow?> FetchTVShowWithEpisodesAsync(int id) => this.videoDatabase.FetchTVShowWithEpisodesAsync(id);
 
-        /// <summary>
-        /// Fetches a TV Show via name.
-        /// </summary>
-        /// <param name="name">TVShow name.</param>
-        /// <returns>TVShow.</returns>
+        /// <inheritdoc/>
         public Task<TVShow?> FetchTVShowViaNameAsync(string name) => this.videoDatabase.FetchTVShowViaNameAsync(name);
 
-        /// <summary>
-        /// Recursivly scan media directories.
-        /// </summary>
-        /// <param name="mediaDirectory">Starting directory.</param>
-        /// <returns>Task.</returns>
+        /// <inheritdoc/>
         public async Task ScanMediaDirectoriesAsync(string mediaDirectory)
         {
             this.logger.Log(LogLevel.Debug, $"ScanMediaDirectories: {mediaDirectory}");
@@ -126,11 +90,7 @@ namespace DrasticMedia.Core.Library
             }
         }
 
-        /// <summary>
-        /// Scan media directory.
-        /// </summary>
-        /// <param name="mediaDirectory">Directory.</param>
-        /// <returns>Task.</returns>
+        /// <inheritdoc/>
         public async Task ScanMediaDirectoryAsync(string mediaDirectory)
         {
             var files = Directory.EnumerateFiles(mediaDirectory);
@@ -141,82 +101,41 @@ namespace DrasticMedia.Core.Library
             }
         }
 
-        /// <summary>
-        /// Fetch an album with tracks by the album id.
-        /// </summary>
-        /// <param name="id">Album Id.</param>
-        /// <returns>AlbumItem.</returns>
+        /// <inheritdoc/>
         public async Task<AlbumItem?> FetchAlbumWithTracksViaIdAsync(int id) => await this.musicDatabase.FetchAlbumWithTracksViaIdAsync(id);
 
-        /// <summary>
-        /// Gets an list of <see cref="ArtistItem"/>.
-        /// </summary>
-        /// <returns>List of ArtistItems.</returns>
+        /// <inheritdoc/>
         public async Task<List<ArtistItem>> FetchArtistsAsync() => await this.musicDatabase.FetchArtistsAsync();
 
-        /// <summary>
-        /// Fetch an artist by the artist id.
-        /// </summary>
-        /// <param name="id">Artist Id.</param>
-        /// <returns>ArtistItem.</returns>
+        /// <inheritdoc/>
         public async Task<ArtistItem?> FetchArtistViaIdAsync(int id) => await this.musicDatabase.FetchArtistViaIdAsync(id);
 
-        /// <summary>
-        /// Fetch an artist with albums by the artist id.
-        /// </summary>
-        /// <param name="id">Artist Id.</param>
-        /// <returns>ArtistItem.</returns>
+        /// <inheritdoc/>
         public async Task<ArtistItem?> FetchArtistWithAlbumsViaIdAsync(int id) => await this.musicDatabase.FetchArtistWithAlbumsViaIdAsync(id);
 
-        /// <summary>
-        /// Gets an <see cref="ArtistItem"/> via their Artist or ArtistGroup name.
-        /// </summary>
-        /// <param name="name">Name to search for.</param>
-        /// <returns>ArtistItem.</returns>
+        /// <inheritdoc/>
         public async Task<ArtistItem?> FetchArtistViaNameAsync(string name) => await this.musicDatabase.FetchArtistViaNameAsync(name);
 
-        /// <summary>
-        /// Gets an list of <see cref="AlbumItem"/>.
-        /// </summary>
-        /// <returns>List of AlbumItems.</returns>
+        /// <inheritdoc/>
         public async Task<List<AlbumItem>> FetchAlbumsAsync() => await this.musicDatabase.FetchAlbumsAsync();
 
-        /// <summary>
-        /// Fetch an album by the album id.
-        /// </summary>
-        /// <param name="id">Album Id.</param>
-        /// <returns>AlbumItem.</returns>
+        /// <inheritdoc/>
         public async Task<AlbumItem?> FetchAlbumViaIdAsync(int id) => await this.musicDatabase.FetchAlbumViaIdAsync(id);
 
-        /// <summary>
-        /// Fetch an track by the track id.
-        /// </summary>
-        /// <param name="id">Track Id.</param>
-        /// <returns>TrackItem.</returns>
+        /// <inheritdoc/>
         public async Task<TrackItem?> FetchTrackViaIdAsync(int id) => await this.musicDatabase.FetchTrackViaIdAsync(id);
 
-        /// <summary>
-        /// Gets an list of <see cref="TrackItem"/>.
-        /// </summary>
-        /// <returns>List of TrackItems.</returns>
+        /// <inheritdoc/>
         public async Task<List<TrackItem>> FetchTracksAsync() => await this.musicDatabase.FetchTracksAsync();
 
-        /// <summary>
-        /// Remove TVShow from database.
-        /// </summary>
-        /// <param name="show">TVShow to remove.</param>
-        /// <returns>Task.</returns>
+        /// <inheritdoc/>
         public async Task RemoveTVShowAsync(TVShow show)
         {
             show = await this.videoDatabase.RemoveTVShowAsync(show).ConfigureAwait(false);
             this.OnRemoveMediaItem(new RemoveMediaItemEventArgs(show));
         }
 
-        /// <summary>
-        /// Remove VideoItem from database.
-        /// </summary>
-        /// <param name="videoItem">VideoItem to remove.</param>
-        /// <returns>Task.</returns>
+        /// <inheritdoc/>
         public async Task RemoveVideoItem(VideoItem videoItem)
         {
             videoItem = await this.videoDatabase.RemoveVideoItemAsync(videoItem).ConfigureAwait(false);
@@ -227,44 +146,28 @@ namespace DrasticMedia.Core.Library
             }
         }
 
-        /// <summary>
-        /// Remove Podcast from Database.
-        /// </summary>
-        /// <param name="podcast">PodcastShowItem</param>
-        /// <returns>Task.</returns>
+        /// <inheritdoc/>
         public async Task RemovePodcast(PodcastShowItem podcast)
         {
             podcast = await this.podcastDatabase.RemovePodcastAsync(podcast).ConfigureAwait(false);
             this.OnRemoveMediaItem(new RemoveMediaItemEventArgs(podcast));
         }
 
-        /// <summary>
-        /// Remove Podcast from Database.
-        /// </summary>
-        /// <param name="podcast">PodcastEpisodeItem</param>
-        /// <returns>Task.</returns>
+        /// <inheritdoc/>
         public async Task RemovePodcastEpisode(PodcastEpisodeItem podcast)
         {
             podcast = await this.podcastDatabase.RemoveEpisodeAsync(podcast).ConfigureAwait(false);
             this.OnRemoveMediaItem(new RemoveMediaItemEventArgs(podcast));
         }
 
-        /// <summary>
-        /// Remove Artist from database.
-        /// </summary>
-        /// <param name="artist">ArtistItem.</param>
-        /// <returns>Task.</returns>
+        /// <inheritdoc/>
         public async Task RemoveArtistAsync(ArtistItem artist)
         {
             artist = await this.musicDatabase.RemoveArtistAsync(artist).ConfigureAwait(false);
             this.OnRemoveMediaItem(new RemoveMediaItemEventArgs(artist));
         }
 
-        /// <summary>
-        /// Remove Album from database.
-        /// </summary>
-        /// <param name="album">AlbumItem.</param>
-        /// <returns>Task.</returns>
+        /// <inheritdoc/>
         public async Task RemoveAlbumAsync(AlbumItem album)
         {
             album = await this.musicDatabase.RemoveAlbumAsync(album).ConfigureAwait(false);
@@ -275,11 +178,7 @@ namespace DrasticMedia.Core.Library
             }
         }
 
-        /// <summary>
-        /// Remove Track from database.
-        /// </summary>
-        /// <param name="track">TrackItem.</param>
-        /// <returns>Task.</returns>
+        /// <inheritdoc/>
         public async Task RemoveTrackAsync(TrackItem track)
         {
             track = await this.musicDatabase.RemoveTrackAsync(track).ConfigureAwait(false);
@@ -290,24 +189,13 @@ namespace DrasticMedia.Core.Library
             }
         }
 
-        /// <summary>
-        /// Fetch all podcasts.
-        /// </summary>
-        /// <returns>List of PodcastShowItem.</returns>
+        /// <inheritdoc/>
         public async Task<List<PodcastShowItem>> FetchPodcastsAsync() => await this.podcastDatabase.FetchShowsAsync().ConfigureAwait(false);
 
-        /// <summary>
-        /// Fetch podcast with episodes.
-        /// </summary>
-        /// <param name="showId">Podcast show id.</param>
-        /// <returns>PodcastShowItem</returns>
+        /// <inheritdoc/>
         public async Task<PodcastShowItem?> FetchPodcastWithEpisodesAsync(int showId) => await this.podcastDatabase.FetchShowWithEpisodesAsync(showId).ConfigureAwait(false);
 
-        /// <summary>
-        /// Add file to database async.
-        /// </summary>
-        /// <param name="path">Path to file.</param>
-        /// <returns>Bool if item was added to the database.</returns>
+        /// <inheritdoc/>
         public async Task<bool> AddFileAsync(string path)
         {
             try
@@ -336,7 +224,11 @@ namespace DrasticMedia.Core.Library
                     }
 
                     var artistName = mP.Artist?.Trim();
-                    var albumArtistName = mP.AlbumArtist?.Trim();
+                    if (artistName == null)
+                    {
+                        return false;
+                    }
+
                     var artist = await this.musicDatabase.FetchArtistViaNameAsync(artistName).ConfigureAwait(false);
                     if (artist is null)
                     {
@@ -349,21 +241,25 @@ namespace DrasticMedia.Core.Library
                     }
 
                     var albumName = mP.Album?.Trim();
-                    var album = await this.musicDatabase.FetchAlbumViaNameAsync(artist.Id, albumName);
-                    if (album is null)
+                    if (albumName != null)
                     {
-                        album = new AlbumItem()
+                        var album = await this.musicDatabase.FetchAlbumViaNameAsync(artist.Id, albumName);
+                        if (album is null)
                         {
-                            ArtistItemId = artist.Id,
-                            Name = albumName,
-                            Year = mP.Year,
-                        };
-                        album = await this.musicDatabase.AddAlbumAsync(album);
-                        this.OnNewMediaItemAdded(new NewMediaItemEventArgs(album));
+                            album = new AlbumItem()
+                            {
+                                ArtistItemId = artist.Id,
+                                Name = albumName,
+                                Year = mP.Year,
+                            };
+                            album = await this.musicDatabase.AddAlbumAsync(album);
+                            this.OnNewMediaItemAdded(new NewMediaItemEventArgs(album));
+                        }
+
+                        mP.AlbumItemId = album.Id;
+                        mP.ArtistItemId = artist.Id;
                     }
 
-                    mP.AlbumItemId = album.Id;
-                    mP.ArtistItemId = artist.Id;
                     mP = await this.musicDatabase.AddTrackAsync(mP).ConfigureAwait(false);
                     this.OnNewMediaItemAdded(new NewMediaItemEventArgs(mP));
                     return mP.Id > 0;
@@ -412,11 +308,7 @@ namespace DrasticMedia.Core.Library
             return false;
         }
 
-        /// <summary>
-        /// Add or update a podcast via a uri.
-        /// </summary>
-        /// <param name="uri">Uri of podcast.</param>
-        /// <returns>Podcast Show Item.</returns>
+        /// <inheritdoc/>
         public async Task<PodcastShowItem?> AddOrUpdatePodcastFromUri(Uri uri)
         {
             try
@@ -447,44 +339,43 @@ namespace DrasticMedia.Core.Library
             }
         }
 
-        /// <summary>
-        /// On New Media Item Added.
-        /// </summary>
-        /// <param name="e">NewMediaItemEventArgs.</param>
-        protected virtual void OnNewMediaItemAdded(NewMediaItemEventArgs e)
+        /// <inheritdoc/>
+        public virtual void OnNewMediaItemAdded(NewMediaItemEventArgs e)
         {
             this.NewMediaItemAdded?.Invoke(this, e);
         }
 
-        /// <summary>
-        /// On Update Media Item Added.
-        /// </summary>
-        /// <param name="e">UpdateMediaItemEventArgs.</param>
-        protected virtual void OnUpdateMediaItemAdded(UpdateMediaItemEventArgs e)
+        /// <inheritdoc/>
+        public virtual void OnUpdateMediaItemAdded(UpdateMediaItemEventArgs e)
         {
             this.UpdateMediaItemAdded?.Invoke(this, e);
         }
 
-        /// <summary>
-        /// On New Media Item Error.
-        /// </summary>
-        /// <param name="e">NewMediaItemEventArgs.</param>
-        protected virtual void OnNewMediaItemError(NewMediaItemErrorEventArgs e)
+        /// <inheritdoc/>
+        public virtual void OnNewMediaItemError(NewMediaItemErrorEventArgs e)
         {
             this.NewMediaItemError?.Invoke(this, e);
         }
 
-        /// <summary>
-        /// On Media Item Removed.
-        /// </summary>
-        /// <param name="e">RemoveMediaItemEventArgs.</param>
-        protected virtual void OnRemoveMediaItem(RemoveMediaItemEventArgs e)
+        /// <inheritdoc/>
+        public virtual void OnRemoveMediaItem(RemoveMediaItemEventArgs e)
         {
             this.RemoveMediaItem?.Invoke(this, e);
         }
 
-        #region Dispose
+        /// <summary>
+        /// Dispose.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
 
+        /// <summary>
+        /// Dispose.
+        /// </summary>
+        /// <param name="disposing">Is Disposing.</param>
         protected virtual void Dispose(bool disposing)
         {
             if (!this.disposedValue)
@@ -497,16 +388,5 @@ namespace DrasticMedia.Core.Library
                 this.disposedValue = true;
             }
         }
-
-        /// <summary>
-        /// Dispose.
-        /// </summary>
-        public void Dispose()
-        {
-            this.Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
-
-        #endregion
     }
 }
