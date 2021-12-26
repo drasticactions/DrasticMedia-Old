37523ae6a1ad143e9130b337938d9a9f585a77be
Tim Miller <innerlogic4321@gmail.com>
2021-12-26 13:03:20 -0500

    Remove VLC

--- DrasticMedia.VLC.App/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace DrasticMedia.VLC.App;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
