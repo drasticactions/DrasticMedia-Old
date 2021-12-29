875ac9944e04ab632bd03091794ebe21c66a7257
Tim Miller <innerlogic4321@gmail.com>
2021-12-28 23:30:31 -0500

    Add Base Token

---
+++ DrasticMedia.Core/Tools/ApiTokens.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="ApiTokens.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrasticMedia.Core.Tools
+{
+    /// <summary>
+    /// API Tokens.
+    /// </summary>
+    public static class ApiTokens
+    {
+        /// <summary>
+        /// Gets the Spotify Client Token.
+        /// </summary>
+        public static string SpotifyClientToken => "";
+
+        /// <summary>
+        /// Gets the Spotify Client Secret Token.
+        /// </summary>
+        public static string SpotifyClientSecretToken => "";
+
+        /// <summary>
+        /// Gets the LastFM Client Token.
+        /// </summary>
+        public static string LastFMClientToken => "";
+
+        /// <summary>
+        /// Gets the LastFM Client Secret Token.
+        /// </summary>
+        public static string LastFMClientSecretToken => "";
+    }
+}
